5a9744546330167098c0d7e535522ed54c26b52b
joacoluna4 <joacolunaf4@gmail.com>
2021-12-04 05:39:38 -0300

    Se agregan titulo y modifican estilos

--- src/App.js
+++ src/App.js
@@ -36,8 +36,9 @@ constructor(){
 
   render() {
     const todos = this.state.todos.map((todo, i)=>{
+      //RETURN DE VARIABLE 'TODOS'
       return(       
-        <div className="col md-4" key={i}>
+        <div className="col-md-4" key={i}>
           
           <div className="card mt-4" >        
           <div className="card-header"> <h3> {todo.title} </h3> 
@@ -57,24 +58,29 @@ constructor(){
         </div>
       )
     }) 
-
+    //RETURN DE LA APP
     return(
       <div className="App">
       <nav className="navbar navbar-dark bg-dark">
         <a href="#" className="text-white App-link"> 
         TASK <span className="badge badge-pill badge-light ml-2"> {this.state.todos.length}</span>
-        </a>
-        
-      </nav>  
-       <div className="row mt-4">{todos}</div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Form onAddTodo={this.handleAddTodo}/>  
-    
-    {/* <div className="col-md-8">
-      <div className="row">
-        { todos }
+        </a>       
+      </nav>
+      <h1 className="title"> TASK MANAGER </h1>
+      <div className="container">
+        <div className="row mt-4" >
+          <div className="col-md-4 text-center">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Form onAddTodo={this.handleAddTodo}/>  
+          </div>
+
+       <div className="col-md-8">
+        <div className="row">
+         {todos}
+        </div>
+       </div>   
+        </div>
       </div>
-    </div> */}
     </div>
     
   );
