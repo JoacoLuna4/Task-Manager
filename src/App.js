feab1ee673960ad5173452ab4eb773c6c253992f
GastonArevalo <53384673+GastonArevalo@users.noreply.github.com>
2021-12-05 15:31:41 -0300

    :art: localStorage todo list

--- src/App.js
+++ src/App.js
@@ -1,98 +1,121 @@
 // import React, {Component, useEffect} from 'react';
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+// import { todos } from './todos.json';
 
-import { todos } from './todos.json'; 
+import Form from "./components/TodoForm";
 
-import Form from './components/TodoForm'
+// console.log(todos);
+// let todosJSON = JSON.stringify(todos);
+// localStorage.setItem("data", todosJSON);
 
-console.log(todos);
-let todosJSON = JSON.stringify(todos)
-localStorage.setItem("data", todosJSON)
-
-
-class App extends Component{
-  
-constructor(){
-  super();
-  this.state = {
-    todos
-  }
-  this.handleAddTodo = this.handleAddTodo.bind(this);
-}
-// useEffect(() => {
-
-// }, []);
-
-  handleRemove(indice){
-      if(window.confirm("Seguro quieres eliminar esta tarea?")){ //Para solicitar confirmacion antes de eliminar, preferente antes que un 'alert' - Se le agrega el window para declarar a React que viene del navegador
-        this.setState({
-          todos: this.state.todos.filter((e , i) =>{             //En vez de recorrer todos los elementos, los filtra si cumplen una condicion
-            return i !== indice;                                 //En este caso si no coincide con el indice que le damos, lo agrega, en el caso que sea igual, lo filtra, no lo agrega al todos
-          })
-        })    
-      }
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
-  handleAddTodo(todo){
+  componentDidMount() {
+    let todoList = JSON.parse(localStorage.getItem("data"));
+
+    if (todoList != null) {
+      this.setState({
+        todos: todoList,
+      });
+    }
+  }
+
+  // useEffect(() => {
+
+  // }, []);
+
+  handleRemove(indice) {
+    let newArray = this.state.todos;
+    newArray.splice(indice, 1);
+    if (window.confirm("Seguro quieres eliminar esta tarea?")) {
+      //Para solicitar confirmacion antes de eliminar, preferente antes que un 'alert' - Se le agrega el window para declarar a React que viene del navegador
+      this.setState({
+        todos: newArray,
+      });
+      localStorage.setItem("data", JSON.stringify(newArray));
+    }
+  }
+
+  handleAddTodo(todo) {
+    let newArray = this.state.todos;
+    newArray.push(todo);
     this.setState({
-      todos: [...this.state.todos, todo]
-    })
+      todos: newArray,
+    });
+    localStorage.setItem("data", JSON.stringify(this.state.todos));
   }
 
   render() {
-    const todos = this.state.todos.map((todo, i)=>{
+    console.log("this.state.todos", this.state.todos);
+    const todos = this.state.todos.map((todo, i) => {
       //RETURN DE VARIABLE 'TODOS'
-      return(       
+      return (
         <div className="col-md-4" key={i}>
-          
-          <div className="card mt-4" >        
-          <div className="card-header"> <h3> {todo.title} </h3> 
-          
-           <span className="badge badge-pill badge-danger ml-2 text-black"> 
-             {todo.priority}
-            </span> 
-          
-          </div> 
-          <div className="card-body"> <p> {todo.description}</p> 
-                                      <p><mark> {todo.responsible} </mark></p>
-          </div>
-          <div className="card-footer">
-              <button className="btn btn-danger" onClick= {this.handleRemove.bind(this, i)} > Delete</button>
+          <div className="card mt-4">
+            <div className="card-header">
+              {" "}
+              <h3> {todo.title} </h3>
+              <span className="badge badge-pill badge-danger ml-2 text-black">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              {" "}
+              <p> {todo.description}</p>
+              <p>
+                <mark> {todo.responsible} </mark>
+              </p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.handleRemove.bind(this, i)}
+              >
+                {" "}
+                Delete
+              </button>
+            </div>
           </div>
         </div>
-        </div>
-      )
-    }) 
+      );
+    });
     //RETURN DE LA APP
-    return(
+    return (
       <div className="App">
-      <nav className="navbar navbar-dark bg-dark">
-        <a href="#" className="text-white App-link"> 
-        TASK <span className="badge badge-pill badge-light ml-2"> {this.state.todos.length}</span>
-        </a>       
-      </nav>
-      <h1 className="title"> TASK MANAGER </h1>
-      <div className="container">
-        <div className="row mt-4" >
-          <div className="col-md-4 text-center">
-            <img src={logo} className="App-logo" alt="logo" />
-            <Form onAddTodo={this.handleAddTodo}/>  
-          </div>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="text-white App-link">
+            TASK{" "}
+            <span className="badge badge-pill badge-light ml-2">
+              {" "}
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+        <h1 className="title"> TASK MANAGER </h1>
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-4 text-center">
+              <img src={logo} className="App-logo" alt="logo" />
+              <Form onAddTodo={this.handleAddTodo} />
+            </div>
 
-       <div className="col-md-8">
-        <div className="row">
-         {todos}
-        </div>
-       </div>   
+            <div className="col-md-8">
+              <div className="row">{todos}</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 export default App;
-
-console.log(todos);
